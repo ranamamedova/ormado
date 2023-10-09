f834d71ee160f47831e666ad1c41c5a7961d7ce6
Tamerlan <daniels-95@inbox.ru>
2023-10-09 17:40:38 +0400

    router/sass/bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+   <>
+      <h1 style={{"textAlign":"center"}} className="mt-5">
+        Ormado DEV
+      </h1>
+   
+   </>
+  )
+}
+
+export default App

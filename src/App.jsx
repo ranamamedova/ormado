550c86ceaf20f135e642bde0642934c81479b132
bediroov <hafizbedirov99@gmail.com>
2023-10-11 14:39:47 +0400

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Home from './components/Home'
 
 const App = () => {
   return (
    <>
       <h1 className='text-center'>
-        Ormado dev Project
+      <Home/>
       </h1>
    
    </>

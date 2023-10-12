80108514c04ae3f26d8d9bf00e209689a3a672e4
Tamerlan <daniels-95@inbox.ru>
2023-10-12 21:54:18 +0400

    enjoyCoffePage

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 
 import Home from './components/Home'
 import Section1 from './components/home/Section1'
+import ProductsDetails from './components/ProductsDetails'
 
 const App = () => {
   return (
    <>
-      <Section1 />
+      {/* <Section1 /> */}
     <Home/>
-   
+   {/* <ProductsDetails/> */}
 
    </>
   )

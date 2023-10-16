7f8f2ad520884b52137e73919281c4c3ba295b2a
bediroov <hafizbedirov99@gmail.com>
2023-10-16 12:46:35 +0400

    last

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import Section1 from './components/home/Section1'
 import Home from "./components/Home"
 
 const App = () => {
   return (
    <>
-      {/* <Section1/> */}
     <Home/>
    
 

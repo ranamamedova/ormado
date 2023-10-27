333963b70d00a7b6b106648aaef417ddb4702359
Tamerlan <daniels-95@inbox.ru>
2023-10-27 18:36:46 +0400

    last version 10.27.23

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
+import { BrowserRouter,Route, Routes } from 'react-router-dom'
 import Home from '../components/Home'
+import Navbar from '../components/Navbar'
+import Contact from '../pages/Contact'
+import Career from '../pages/Career'
+import Product from '../pages/Product'
 
 const AppRouter = () => {
   return (
-    <div>
-
-
-      <Home/>
-    </div>
-  )
+   <>
+   <BrowserRouter>
+   <Navbar/>
+   <Routes>
+    <Route  path='/' element={<Home/>}></Route>
+    <Route  path='/contact' element={<Contact/>}></Route>
+    <Route  path='/career' element={<Career/>}></Route>
+    <Route  path='/product' element={<Product/>}></Route>
+   </Routes>
+   </BrowserRouter>
+   </>
+  )   
 }
  
 export default AppRouter

81b9d9ad73976781943d02deba0a7cb792822503
Tamerlan <daniels-95@inbox.ru>
2023-11-02 21:20:04 +0400

    Sign In change 11.02.23

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,16 +1,42 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import login from "../assets/img/login.png"
 import logo from "../assets/img/logo.png"
 const SignIn = () => {
   return (
     <>
+        <section className="singin">
         <div className="container-fluid">
             <div className="row">
-                <div className="col-5">
-                <img src={login} alt=""  className='img-fluid my-5'/>
+                <div className="col-12 col-sm-5  ">
+                <div className="signImg">
+                <img src={login} alt=""  className='img-fluid'/>
+                <div className="logoTitle ">
+                 <div className="logoImg">
+                 <Link to="/">
+                 <img src={logo} alt="" className='mt-3 mb-5'  />
+                 </Link>
+                 </div>
+                   <h2 >
+                   WELCOME TO ORMADO KAFFEEHAUS
+                   </h2>
+                </div>
+                </div>
+                </div>
+                <div className="col-12 col-sm-5 " >
+                <div className="formBox">
+                <div className="formTitle">
+                <h5>Welcome back!</h5>
+                <p>Meet the good taste today</p>
+                </div>
+                  <form >
+                     
+                  </form>
+                </div>
                 </div>
             </div>
         </div>
+        </section>
     </>
   )
 }

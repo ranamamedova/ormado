2663b5e51f0c2f9ece2424408e217554e4620325
Tamerlan <daniels-95@inbox.ru>
2023-11-11 15:54:52 +0400

    Our story

--- src/pages/OurStory.jsx
+++ src/pages/OurStory.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import story from "../assets/img/ourstory.png"
+import blast from "../assets/img/blast.png"
 const OurStory = () => {
   return (
-    <div>OurStory</div>
+    <>
+  <div className="container-fluid">
+  <img src={story} alt="" className='img-fluid' style={{width:"100%"}} />
+    <div className="row">
+      <div className="col-5 bg-primary mt-5">
+        <img src={blast} alt="" className='img-fluid' />
+      </div>
+    </div>
+  </div>
+    </>
   )
 }
 

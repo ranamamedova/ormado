dad800787b33f287eadcf82d6550bcce78637ad7
Selcan <selcanasirova@gmail.com>
2023-11-21 14:54:50 +0400

    update

---
+++ src/pages/ProductDetails.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductDetails = () => {
+  return (
+    <div>
+        <div className="product-details">
+            <div className="product-details-img">
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ProductDetails

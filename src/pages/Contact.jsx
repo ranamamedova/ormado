6bc1f1ace443e214e1acfdc62e9fb9c6acb8a740
Tamerlan <daniels-95@inbox.ru>
2023-11-23 15:35:15 +0400

    change contact register

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,24 +4,24 @@ import { useState } from "react";
 import { useLocation, useNavigate } from "react-router";
 
 const Contact = () => {
-  const location = useLocation();
-  const [login, setLogin] = useState(false);
-  const email = localStorage.getItem("userEmail");
-  const password = localStorage.getItem("userPassword");
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (email && password) {
-      setLogin(true);
-    } else {
-      setLogin(false);
-      if (location.pathname === "/contact") {
-        navigate("/signin");
-      }
-    }
-  }, []);
+  // const location = useLocation();
+  // const [login, setLogin] = useState(false);
+  // const email = localStorage.getItem("userEmail");
+  // const password = localStorage.getItem("userPassword");
+  // const navigate = useNavigate();
+  // useEffect(() => {
+  //   if (email && password) {
+  //     setLogin(true);
+  //   } else {
+  //     setLogin(false);
+  //     if (location.pathname === "/contact") {
+  //       navigate("/signin");
+  //     }
+  //   }
+  // }, []);
   return (
     <>
-      {
+      {/* {
         login && <div>
         <iframe
           src="https://my.atlist.com/map/f77aa50c-84cd-47b8-a0e1-8b31387435e8?share=true"
@@ -33,7 +33,8 @@ const Contact = () => {
           allowfullscreen
         ></iframe>
       </div>
-      }
+      } */}
+      
     </>
   );
 };

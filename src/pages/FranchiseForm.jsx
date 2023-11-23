e9f9bb16d9e56a4f1bd6798ee41a9496c9d5e01c
Tamerlan <daniels-95@inbox.ru>
2023-11-23 20:34:59 +0400

    franchise form

--- src/pages/FranchiseForm.jsx
+++ src/pages/FranchiseForm.jsx
@@ -5,8 +5,8 @@ const FranchiseForm = () => {
     <>
       <section className="franchiseform">
         <div className="container">
-          <div className="row  justify-content-center">
-            <div className="col-12 col-sm-6  ">
+          <div className="row  justify-content-center g-5">
+            <div className="col-12 col-sm-6 ">
               <div className="franchiseFormImg">
                 <div className="img">
                   <img src={franchise} alt="" className="img-fluid" />
@@ -20,7 +20,7 @@ const FranchiseForm = () => {
                 </div>
               </div>
             </div>
-            <div className="col-12 col-sm-6  ">
+            <div className="col-12 col-sm-6 ">
               <div className="formTitle mb-5 text-center">
                 <h4>
                   ORMADO
@@ -35,10 +35,62 @@ const FranchiseForm = () => {
                   Ut enim ad minim veniam, quis nostrud exercitation
                 </p>
               </div>
-              <div className="formInput">
-                <input type="text" />
-                <label>First Name</label>
-              </div>
+              <form>
+                <div className="row  formRow">
+                  <div className="col-6">
+                    <div className="formInput">
+                      <input type="text" />
+                      <label>First Name</label>
+                    </div>
+                  </div>
+                  <div className="col-6">
+                    <div className="formInput">
+                      <input type="text" />
+                      <label>Last Name</label>
+                    </div>
+                  </div>
+                  <div className="col-12 my-5">
+                    <div className="formInput">
+                      <input type="text" />
+                      <label>Email</label>
+                    </div>
+                  </div>
+                  <div className="col-6 mb-5">
+                    <div className="formInput ">
+                      <input type="text" />
+                      <label>Phone</label>
+                    </div>
+                  </div>
+                  <div className="col-6">
+                    <div className="formInput">
+                      <input type="text" />
+                      <label>City</label>
+                    </div>
+                  </div>
+                  <div className="col-6">
+                    <div className="formInput">
+                      <input type="text" />
+                      <label>Country</label>
+                    </div>
+                  </div>
+       
+                </div>
+                <div className="d-flex justify-content-between my-4">
+                    <div className="checkBox">
+                      <input type="checkbox" name="" id="" className="me-2 mt-1" />
+                    </div>
+                    <p>
+                      Your personal data will be used to process your enquiry,
+                      support your experience throughout this website, and for
+                      other purposes described in our <span>Privacy policy.</span>
+                    </p>
+                  </div>
+                   <div className="d-flex justify-content-center">
+             
+                   <button className="text-center submitBtn w-100">SUBMIT</button>
+                  
+                   </div>
+              </form>
             </div>
           </div>
         </div>

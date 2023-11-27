4221dc3d4a4d37a36ce13c1867887cd67c5821d4
Rana Mamedova <121027771+ranamamedova@users.noreply.github.com>
2023-11-27 15:23:57 +0400

    end

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -22,13 +22,14 @@ const Contact = () => {
   return (
     <>
 <div className="diascuss text-light ">
-  <div className="row mx-5">
-    <div className="col-12 col-sm-6 col-md-4 my-5 mx-5">
-      <h1 >contact us</h1>
+  <div className="row ">
+    <div className="col-12 col-sm-6 col-md-4 my-5 order-md-1 order-2 ">
+      <h1 >contact <br /> us</h1>
      <h2>Have a project! Let’s diascuss </h2>
      <p >Thank you for getting in touch! <br /> Kindly. <br /> Fill the form, have a great day!</p>
     </div>
-     <div className="col-12 col-sm-6 col-md-4 my-5 mx-5">
+     <div className="col-12 col-sm-6 col-md-4 my-5 order-md-2 order-1">
+      <div className="left-one ">
       <div className="both ">
         <div className="row">
           <div className="col-12 col-sm-12 col-md-6">
@@ -52,7 +53,7 @@ const Contact = () => {
           <input type="text" placeholder="Company Name"/>
           </div>
           <div className="col-12 col-sm-12 col-md-6">
-          <select name="interested" id="interested">
+          <select name="interested" id="interested" >
           <option disabled selected value="" >Interested In</option>
         <option value="A">A</option>
         <option value="B">B</option>
@@ -64,8 +65,9 @@ const Contact = () => {
 
       </div>
       <div className="endl d-flex flex-directions-column">
-      <input type="text" name="" id="" placeholder="Message" className="mb-3"/>
+      <input type="text" name="" id="" placeholder="Message" className="message "/>
         <button type="submit">Submit</button>
+      </div>
       </div>
     </div>
   </div>

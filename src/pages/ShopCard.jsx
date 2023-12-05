f921eab176afb886164b0600b2b6782e678c7db5
Rana Mamedova <121027771+ranamamedova@users.noreply.github.com>
2023-12-05 15:44:33 +0400

    shopCard

--- src/pages/ShopCard.jsx
+++ src/pages/ShopCard.jsx
@@ -43,7 +43,7 @@ const ShopCard = ({onDelete,classNames}) => {
 
       <div className="shopcard">
         <div className="row mx-5 my-5">
-          <div className="col-12 col-sm-12 col-md-9">
+          <div className="col-12 col-sm-12 col-md-9 mb-5">
             <table>
               <thead>
                 <tr>
